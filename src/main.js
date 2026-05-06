173bfa30672a132aef156c9a6c94eab919be9eae
HaikalRabbani <muhaikalrabbani@gmail.com>
2026-05-06 09:51:27 +0700

    server di vite

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
-import App from './App.vue'
+import app from './App.vue'
 import router from './router'
 
 const vueApp = createApp(app)
